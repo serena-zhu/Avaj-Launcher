1ddac97759aaf0d2dff70017077946a476b74fce
Yan jun Zhu <itsserzy@gmail.com>
2018-08-24 18:10:04 -0700

    started exception handling and file reading in Simulator main method, also added scenario text file

--- Makefile.py
+++ Makefile.py
@@ -1,7 +1,14 @@
-import os
+import os, sys
+
 print("Cleaning...")
 os.system('find . -name "*.class" -type f -print0 | xargs -0 /bin/rm -f')
 print("Recompiling...")
 os.system('javac launcher/avaj/simulator/Simulator.java')
 print("Executing...")
-os.system('java launcher/avaj/simulator/Simulator')
+
+if len(sys.argv) > 1:
+	filename = sys.argv[1]
+else:
+	filename = ""
+
+os.system('java launcher/avaj/simulator/Simulator ' + filename)
--- launcher/avaj/simulator/Simulator.java
+++ launcher/avaj/simulator/Simulator.java
@@ -2,29 +2,49 @@ package launcher.avaj.simulator;
 
 import launcher.avaj.simulator.aircraft.AircraftFactory;
 import launcher.avaj.simulator.aircraft.Flyable;
-// import simulator.WeatherTower;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.ArrayIndexOutOfBoundsException;
+import java.lang.InterruptedException;
 
 public class Simulator {
-	public static void main(String[] args) {
-		WeatherTower weatherTower = new WeatherTower();
+	public static void main(String[] args) throws InterruptedException {
+		try {
+		
+			BufferedReader reader = new BufferedReader(new FileReader(args[0])); 
+			String line;
+			while ((line = reader.readLine()) != null) {
+				System.out.println(line);
+			}
 
-		// loop through all aircrafts...
+			WeatherTower weatherTower = new WeatherTower();
 
-		Flyable flyable;
+			// loop through all aircrafts...
 
-		String type = "balloon";
-		System.out.printf("Will try to create a %s...%n", type);
-		flyable = AircraftFactory.newAircraft(type, "hi", 36, 80, 18);
-		flyable.registerTower(weatherTower);
+			Flyable flyable;
 
-		type = "jetplane";
-		System.out.printf("Will try to create a %s...%n", type);
-		flyable = AircraftFactory.newAircraft(type, "hiagain", 12, 600, 18);
-		flyable.registerTower(weatherTower);
-		// all aircrafts created and registered...
+			String type = "balloon";
+			System.out.printf("Will try to create a %s...%n", type);
+			flyable = AircraftFactory.newAircraft(type, "hi", 36, 80, 18);
+			flyable.registerTower(weatherTower);
 
-		//change weather each day
-		weatherTower.changeWeather();
+			type = "jetplane";
+			System.out.printf("Will try to create a %s...%n", type);
+			flyable = AircraftFactory.newAircraft(type, "hiagain", 12, 600, 18);
+			flyable.registerTower(weatherTower);
+			// all aircrafts created and registered...
 
+			//change weather each day
+			weatherTower.changeWeather();
+		} catch (FileNotFoundException e) {
+			System.out.printf("File %s not found.%n", args[0]);
+		} catch (IOException e) {
+			System.out.printf("Error reading file %s.%n", args[0]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Please provide scenario file for simulation.");
+		}
 	}
 }
