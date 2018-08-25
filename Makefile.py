70ee9a002c16a99404e2422498d877f9d06baec8
Yan jun Zhu <itsserzy@gmail.com>
2018-08-25 15:41:11 -0700

    updated makefile

--- Makefile.py
+++ Makefile.py
@@ -1,14 +1,31 @@
 import os, sys
 
-print("Cleaning...")
-os.system('find . -name "*.class" -type f -print0 | xargs -0 /bin/rm -f')
-print("Recompiling...")
-os.system('javac launcher/avaj/simulator/Simulator.java')
-print("Executing...")
+filename = ""
+clean = 'find . -name "*.class" -type f -print0 | xargs -0 /bin/rm -f'
+make = 'javac launcher/avaj/simulator/Simulator.java'
+run = 'java launcher/avaj/simulator/Simulator '
+usage = "usage: python3 Makefile.py [make | run | clean | makere] (filename)"
 
-if len(sys.argv) > 1:
-	filename = sys.argv[1]
+if len(sys.argv) >= 3:
+	filename = sys.argv[2]
+
+if len(sys.argv) >= 2:
+	if sys.argv[1] == "make":
+		print("Compiling...")
+		os.system(make)	
+	elif sys.argv[1] =="run":
+		print("Executing...")
+		os.system(run + filename)	
+	elif sys.argv[1] == "clean":
+		print("Cleaning...")
+		os.system(clean)
+	elif sys.argv[1] == "makere":
+		print("Cleaning and recompiling...")
+		os.system(clean + "&&" + make)
+	else:
+		print(usage)
 else:
-	filename = ""
+	print(usage)
 
-os.system('java launcher/avaj/simulator/Simulator ' + filename)
+
+
